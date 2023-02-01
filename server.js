54200e31cbcc42de01397448578d463b0ea661d2
Isamara Galvão <isamaragsantos@gmail.com>
2023-01-31 23:07:31 -0300

    config server

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-const http = require('http')
-const port = 3000
+import app from './src/app.js';
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('Ola, tudo bem? Tudo funcionando por aqui! =)')
-})
+const port = process.env.PORT || 3000;
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log('Escutando requisição na porta 3000.');
-})
+});
 
